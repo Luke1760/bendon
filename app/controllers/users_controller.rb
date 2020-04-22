6f75e07950dc6050c801276a844c4cfcc2b7566a
lukeemail <kjd1760@gmail.com>
2020-04-22 16:40:44 +0800

    issue #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,38 @@
 class UsersController < ApplicationController
 
   def login
+    @user = User.new
   end
 
   def sign_up
       @user = User.new
   end
 
+  def sign_in
+    user = User.find_by(email: user_params[:email],
+                        password: user_params[:password])
+    if user
+      session[:ccc9527] = user.email
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
     def registration
       @user = User.new(user_params)
       if @user.save
-        # 成功
-        # TODO: 密碼加密
-        # TODO: 幫登入
-        redirect_to '/'
+        session[:ccc9527] = @user.email
+        redirect_to root_path
       else
         # 失敗
         render :sign_up
       end
     end
-
+      def logout
+        session[:ccc9527] = nil
+        redirect_to root_path
+      end
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
+  root 'welcome#index'
+
   get '/login', to: 'users#login'
+  post '/login', to: 'users#sign_in'
+  delete '/logout', to: 'users#logout'
+
   get '/sign_up', to: 'users#sign_up'
   post '/sign_up', to: 'users#registration'
-
-  root 'welcome#index'
 end
