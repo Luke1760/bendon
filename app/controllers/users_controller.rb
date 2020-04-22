b834827788990a58aa8fcd9f7f48baa539b45974
lukeemail <kjd1760@gmail.com>
2020-04-22 12:06:26 +0800

    issue #2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,16 @@ class UsersController < ApplicationController
 
   def login
   end
-  
+
+  def sign_up  
+  end
+
+  def registration
+    user = params[:user]
+    if user[:password] == user[:password_confirm]
+      redirect_to '/login'
+    else
+      redirect_to '/sign_up'
+    end
+  end
 end
